a41766ebfa8af1aa7e9663d22435ac8540f45254
José Eduardo Ortiz Espinoza <kanon488@hotmail.com>
2024-11-04 21:53:11 -0600

    feat(db): add DbContext configuration for database connection

---
+++ AppCode/DBContext/DBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiPOS.AppCode.DBContext
+{
+    public partial class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base (options)
+        {
+                
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
